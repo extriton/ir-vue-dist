7d20ada3ef8432c414d60e40926fa146d023c5df
unknown <you@example.com>
2020-02-28 18:53:11 +0300

    bugfix

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
@@ -7,5 +8,18 @@ app.use('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '/dist') + '/index.html')
 })
 const port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
+*/
+
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+app = express()
+app.use(serveStatic(__dirname))
+app.use('/*', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
